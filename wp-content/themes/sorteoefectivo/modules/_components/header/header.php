8ab4dcf67b8c5e34bcd542a461d661760b1e249d
Alberto Hrtzt <me@albertohartzet.com>
2018-08-08 21:23:21 -0500

    Layout Skew Historias

--- wp-content/themes/sorteoefectivo/modules/_components/header/header.php
+++ wp-content/themes/sorteoefectivo/modules/_components/header/header.php
@@ -1,10 +1,14 @@
 <header id="main-header" class="wk-section">
     <div class="wk-cols">
         <div class="wk-col">
-            <a href="<?php bloginfo( 'url' ); ?>" id="logo-bancoppel" class="logo"><img width="400" src="<?php echo get_option('wk_custom_logo_main'); ?>" alt="Bancoppel"></a>
+            <a href="<?php bloginfo( 'url' ); ?>" id="logo-bancoppel" class="logo">
+                <img width="400" src="<?php echo get_option('wk_custom_logo_main'); ?>" alt="Bancoppel">
+            </a>
         </div>
         <div class="wk-col wk-text-right">
-            <a href="<?php bloginfo( 'url' ); ?>" id="logo-sorteo-efectivo" class="logo"><img width="160" src="<?php echo get_option('wk_custom_logo_alt'); ?>" alt="Sorteo Efectivo"></a>
+            <a href="<?php bloginfo( 'url' ); ?>" id="logo-sorteo-efectivo" class="logo">
+                
+            </a>
         </div>
     </div>
 </header>
--- wp-content/themes/sorteoefectivo/modules/home/home.php
+++ wp-content/themes/sorteoefectivo/modules/home/home.php
@@ -1,4 +1,4 @@
-<section id="header-copy" class="wk-section">
+<section id="section-header-copy" class="wk-section">
     <div class="wk-section-wrap">
 
         <div class="wk-cols">
@@ -6,7 +6,7 @@
                 <div class="container">
                     <h4 class="ui-title-extra-big">
                         <span class="title-a-1">¡Quiero ganar</span> 
-                        <strong class="title-a-2 ui-title-extra-big">$100,000</strong>
+                        <span class="title-a-2 ui-title-extra-big"><strong>$100,000</strong></span>
                         <span class="title-a-3">cada mes!</span>
                     </h4>
                     <h2 class="ui-title ui-text-main">
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="content-column wk-col">
-                <?php img( 'cartera.png'); ?>
+                <img width="300" src="<?php echo get_option('wk_custom_logo_alt'); ?>" alt="Sorteo Efectivo">
             </div>
         </div>
 
@@ -31,189 +31,53 @@
 </section>
 
 
-<main id="slider-historias">
+<section id="section-history-excerpt" class="wk-section">
 
-    <section id="slider-historias" class="wk-section">
-    
-        <div class="slider-historias-nav">
+    <?php $wp_query = new WP_Query( 'post_type=post&posts_per_page=3' ); ?>
 
-            <?php 
-                $args = array(
-                    'post_type'		=>	'post',
-                    'order'         => 'DESC'
-                );
-                $wp_query = new WP_Query( $args );
-            ?>
+    <div class="histories">
+       
+        <?php if( $wp_query->have_posts() ) : $r; while( $wp_query->have_posts() ) : $r++; $wp_query->the_post(); ?>
             
-            <?php if( $wp_query->have_posts() ) : $r = 0; while( $wp_query->have_posts() ) : $r++; $wp_query->the_post(); ?>
-            
-                <div id="slide-<?= $post->post_name ?>" class="slide skew">
-                    <div class="content">
-                        <style id="slide-historia-nav-bg-<?= $r ?>">
-                            #slide-<?= $post->post_name ?> .bg { background-image: url(<?= $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>); }
-                        </style>
-                        <div class="bg"></div>
-                        <div class="excerpt">
-                            <div class="top">
-
-                                <?php
-                                    $nombre = get_the_title();
-                                    $nombreapellido = explode( ' ', $nombre );
-                                ?>
-
-                                <h1 class="nombre ui-title"><strong><?= $nombreapellido[0] ?></strong><?= $nombreapellido[1] ?></h1>
-                                <h2 class="ciudad ui-help-text"><?php the_field( 'estado' ); ?></h2>
-                            </div>
-                            <div class="permalink ui-title wk-text-uppercase ui-text-secondary">VER HISTORIA</div>
-                        </div>
+            <a id="history-<?= $post->post_name ?>" class="history" href="<?php the_permalink(); ?>" >
+                <style id="css-history-<?= $post->post_name ?>">
+                    #history-<?= $post->post_name ?>  { background-image: url(<?= $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>); }
+                </style>
+                <div class="background"></div>
+                <?php $nombre = get_the_title(); $nombreApellido = explode( ' ', $nombre ); ?>
+                <div class="excerpt">
+                    <div class="info">
+                        <h1 class="name"><strong><?= $nombreApellido[0]; ?></strong><span><?= $nombreApellido[1]; ?></span> </h1>
+                        <h2 class="state"><strong><?php the_field( 'estado' ); ?></strong></h2>
                     </div>
+                    <span class="permalink">VER HISTORIA</span>
                 </div>
-            
-            <?php endwhile; wp_reset_query(); endif; ?>
+            </a>
+        
+        <?php endwhile; endif; ?>
 
+    </div>
+
+    <div class="share-links">
+        <a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+        <a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+    </div>
+
+</section>
+
+
+
+<section id="section-video" class="wk-section">
+    
+    <style id="css-video">
+        #section-video .video-thumbnail { background-image: url(<?php $thumbnail = get_field( 'video_thumbnail' ); echo $thumbnail['sizes']['large'] ?>); }
+    </style>
+    <a class="video-thumbnail" data-fancybox href="<?php the_field( 'video' ); ?>">
+        <div class="gradient"></div>
+        <div class="copy">
+            <div class="ui-title">Descubre la historia de <strong><?php the_title(); ?></strong> <span class="icon-play"><span class="fa fa-play"></span></span></div>
         </div>
-        
-    </section>
+    </a>
 
-    <div class="slider-historias-content">
+</section>
 
-         <?php 
-                
-            $args = array(
-                'post_type'		=>	'post',
-                'order'         => 'DESC'
-            );
-        
-            $wp_query = new WP_Query( $args );
-        
-        ?>
-            
-        <?php if( $wp_query->have_posts() ) : $r = 0; while( $wp_query->have_posts() ) : $r++; $wp_query->the_post(); ?>
-            
-            <div id="historia-<?= $r ?>" class="slide">
-
-                <section id="section-video" class="wk-section">
-                    
-                    <style id="css-video">
-                        #historia-<?= $r ?> .video-thumbnail { background-image: url(<?php $thumbnail = get_field( 'video_thumbnail' ); echo $thumbnail['sizes']['large'] ?>); }
-                    </style>
-                    <a class="video-thumbnail" data-fancybox href="<?php the_field( 'video' ); ?>">
-                        <div class="copy">
-                            <div class="ui-title">Descubre la historia de <strong><?php the_title(); ?></strong> <span class="icon-play"><span class="fa fa-play"></span></span></div>
-                        </div>
-                    </a>
-
-                </section>
-
-                <div class="wk-wrap-900">
-                    <section id="section-content" class="wk-section">
-                        <div class="wk-section-wrap">
-    
-                            <div class="wk-cols">
-                            
-                                <div class="content-column content-column-left wk-col wk-text-right">
-    
-                                    <?php
-                                        $nombre = get_the_title();
-                                        $nombreapellido = explode( ' ', $nombre );
-                                    ?>
-    
-                                    <h1 class="nombre ui-title"><strong><?= $nombreapellido[0] ?></strong><?= $nombreapellido[1] ?></h1>
-                                    <?php the_field( 'profesion' ); ?>
-                                    <?php the_field( 'extracto' ); ?>
-                            
-                                </div>
-    
-                                <div class="content-column content-column-center wk-col-2">
-                                    <span class="ui-hr-vertical"></span>
-                                </div>
-                            
-                                <div class="content-column content-column-right wk-col">
-    
-                                    <?php if( have_rows( 'parrafos' ) ) : while( have_rows( 'parrafos' ) ) : the_row( 'parrafos' ); ?>
-                                        <?php the_sub_field( 'parrafo' ); ?>
-                                    <?php endwhile; endif; ?>
-                            
-                                </div>
-                            
-                            </div>
-    
-                        </div>
-                    </section>
-                </div>
-
-                <section id="section-quote" class="wk-section section-content">
-                    <div class="wk-section-wrap">
-
-                        <div class="quote"><?php the_field( 'quote' ); ?></div>
-
-                    </div>
-                </section>
-
-                <section id="relacionados" class="wk-section">
-                    <div class="wk-section-wrapp">
-
-                        <div class="wk-cols">
-                        
-                            <!-- <div class="wk-col-1">
-                                <div class="container">
-                                    <p class="ui-title-big">
-                                        <strong>CONOCE LAS HISTORIAS</strong><span>DE OTROS GANADORES</span>
-                                    </p>
-                                </div>
-                            </div> -->
-                        
-                            <div class="wk-col">
-                                
-                                <div class="slider">
-
-                                    <?php 
-                                        $args = array(
-                                            'post_type'		=>	'post',
-                                            'order'         => 'DESC'
-                                        );
-                                        $wp_query = new WP_Query( $args );
-                                    ?>
-                                    
-                                    <?php if( $wp_query->have_posts() ) : $r = 0; while( $wp_query->have_posts() ) : $r++; $wp_query->the_post(); ?>
-                                    
-                                        <div id="slide-<?= $post->post_name ?>" class="slide skew">
-                                            <div class="content">
-                                                <style id="slide-historia-nav-bg-<?= $r ?>">
-                                                    #slide-<?= $post->post_name ?> .bg { background-image: url(<?= $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>); }
-                                                </style>
-                                                <div class="bg"></div>
-                                                <div class="excerpt">
-                                                    <div class="top">
-
-                                                        <?php
-                                                            $nombre = get_the_title();
-                                                            $nombreapellido = explode( ' ', $nombre );
-                                                        ?>
-
-                                                        <h1 class="nombre ui-title"><strong><?= $nombreapellido[0] ?></strong><?= $nombreapellido[1] ?></h1>
-                                                        <h2 class="ciudad ui-help-text"><?php the_field( 'estado' ); ?></h2>
-                                                    </div>
-                                                    <div class="permalink ui-title wk-text-uppercase ui-text-secondary">VER HISTORIA</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    
-                                    <?php endwhile; wp_reset_query(); endif; ?>
-
-                                </div><!-- slider historias nav-->
-                            
-                            </div>
-                        
-                        </div>
-
-                    </div>
-                </section>
-
-            </div>
-
-        <?php endwhile; wp_reset_query(); endif; ?>
-
-    </div>       
-
-</main>
